c15dc0d03794ea785f18591ed549ac5ea777e2ff
kkwongs <kihyun5741@gmail.com>
2022-03-28 11:20:23 +0900

    Add badge

--- js/main.js
+++ js/main.js
@@ -14,3 +14,25 @@ searchInputEl.addEventListener('blur', function () {
   searchEl.classList.remove('focused');
   searchInputEl.setAttribute('placeholder', '');
 });
+
+const badgeEl = document.querySelector('header .badges');
+
+window.addEventListener(
+  'scroll',
+  _.throttle(function () {
+    console.log(window.scrollY);
+    if (window.scrollY > 500) {
+      // gsap.to(요소, 지속시간(s), 옵션);
+      gsap.to(badgeEl, 0.6, {
+        opacity: 0,
+        display: 'none',
+      });
+    } else {
+      gsap.to(badgeEl, 0.6, {
+        opacity: 1,
+        display: 'block',
+      });
+    }
+  }, 300)
+);
+// _.throttle(함수, 시간(ms))
