2a1a7101049670f5c719c3c0719fa9bbd46714a1
kkwongs <kihyun5741@gmail.com>
2022-03-29 15:54:33 +0900

    Add notice with swiperjs

--- js/main.js
+++ js/main.js
@@ -20,7 +20,6 @@ const badgeEl = document.querySelector('header .badges');
 window.addEventListener(
   'scroll',
   _.throttle(function () {
-    console.log(window.scrollY);
     if (window.scrollY > 500) {
       // gsap.to(요소, 지속시간(s), 옵션);
       gsap.to(badgeEl, 0.6, {
@@ -44,4 +43,44 @@ fadeEls.forEach(function (fadeEl, index) {
     delay: (index + 1) * 0.5,
     opacity: 1,
   });
+});
+
+// new Swiper(선택자, 옵션)
+new Swiper('.notice-line .swiper', {
+  direction: 'vertical',
+  autoplay: true,
+  loop: true,
+});
+new Swiper('.promotion .swiper', {
+  slidesPerView: 3, // 한번에 보여줄 슬라이드 개수
+  spaceBetween: 10, // 슬라이드 사이 여백
+  centeredSlides: true, // 1번 슬라이드가 가운데 보이기
+  loop: true,
+  // autoplay: {
+  //   delay: 5000,
+  // },
+  pagination: {
+    el: '.promotion .swiper-pagination', // 페이지 번호 요소 선택자
+    clickable: true, // 사용자의 페이지 번호 요소 제어 가능 여부
+  },
+  navigation: {
+    prevEl: '.promotion .swiper-prev',
+    nextEl: '.promotion .swiper-next',
+  },
+});
+
+const promotionEl = document.querySelector('.promotion');
+const promotionToggleBtn = document.querySelector('.toggle-promotion');
+const promotionToggleBtnIcon =
+  promotionToggleBtn.querySelector('.material-icons');
+let isHidePromotion = false;
+promotionToggleBtn.addEventListener('click', function () {
+  isHidePromotion = !isHidePromotion;
+  if (isHidePromotion) {
+    promotionEl.classList.add('hide');
+    promotionToggleBtnIcon.textContent = 'download';
+  } else {
+    promotionEl.classList.remove('hide');
+    promotionToggleBtnIcon.textContent = 'upload';
+  }
 });
